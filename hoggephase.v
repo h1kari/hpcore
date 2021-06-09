cd218f8f743656a2b88b8e55cf28671061ef220a
zerotoasic <zerotoasic@zerotoasic.vm.com>
2021-06-10 01:21:31 +0200

    added ability to select detectors

--- hoggephase.v
+++ hoggephase.v
@@ -4,34 +4,22 @@ module hp_mod #(
     parameter INVERT = 0	
 ) (
     input  wire CK,
+    output reg  CK2 = 0,
     input  wire VCC,
     output reg Data = 0
 );
 
-generate
-    if (INVERT)
-    begin
-        // create ring oscillator with data at f/2
-        always @(negedge CK)
-        begin
-            if(VCC == 0)
-                #0.2 Data <= 0;
-            else
-                #0.2 Data <= ~Data;
-        end
+always @(posedge CK)
+    CK2 <= !CK2;
+
+// create ring oscillator with data at f/2
+always @(posedge CK or VCC)
+    if (CK2 == INVERT) begin
+        if(VCC == 0)
+            #0.2 Data <= 0;
+        else
+            #0.2 Data <= ~Data;
     end
-    else
-    begin
-        // create ring oscillator with data at f/2
-        always @(posedge CK)
-        begin
-            if(VCC == 0)
-                #0.2 Data <= 0;
-            else
-                #0.2 Data <= ~Data;
-        end
-    end
-endgenerate
 
 endmodule
 
@@ -40,6 +28,7 @@ module hp_pd #(
     parameter INVERT = 0	
 ) (
     input  wire CK,
+    input  wire CK2,
     input  wire Data,
     output wire Alarm    
 );
@@ -48,24 +37,13 @@ module hp_pd #(
 // alarm = !x * !y = !(Data ^ B) * !(B ^ A) 
 reg B = 0, A = 0;
 
-generate
-    if (INVERT)
-    begin
-        always @(negedge CK)
-            #0.1 B <= Data;
+always @(posedge CK)
+    if (CK2 == INVERT)
+        #0.1 B <= Data;
 
-        always @(posedge CK)
-            #0.1 A <= B;
-    end
-    else
-    begin
-        always @(posedge CK)
-            #0.1 B <= Data;
-
-        always @(negedge CK)
-            #0.1 A <= B;
-    end
-endgenerate
+always @(posedge CK)
+    if (CK2 != INVERT)
+        #0.1 A <= B;
 
 wire Y, X;
 assign Y = Data ^ B;
@@ -85,14 +63,17 @@ module hoggephase #(
 );
 
 wire Data;
+wire CK2;
 hp_mod #(INVERT) hp_mod (
     .CK(CK),
+    .CK2(CK2),
     .VCC(VCC),
     .Data(Data)
 );
 
 hp_pd #(INVERT) hp_pd (
     .CK(CK),
+    .CK2(CK2),
     .Data(Data ^ glitch),
     .Alarm(Alarm)
 );
--- wb_hp.v
+++ wb_hp.v
@@ -4,31 +4,78 @@
 `define IO_INPUT  1'b1
 `define IO_OUTPUT 1'b0
 `define SET_IO_OUTPUT(sig, pin) \
-    gpio_enb[pin] <= {16{`IO_OUTPUT}}; \
-    gpio_o[pin] <= sig
+    assign gpio_enb[pin] = {16{`IO_OUTPUT}}; \
+    assign gpio_o[pin] = sig
 `define SET_IO_INPUT(sig, pin) \
-    gpio_enb[pin] <= {16{`IO_INPUT}}; \
-    sig <= gpio_i[pin] 
+    assign gpio_enb[pin] = {16{`IO_INPUT}}; \
+    assign sig = gpio_i[pin] 
 `define SET_IO_NA(pin) \
-    gpio_enb[pin] <= {16{`IO_INPUT}}
+    assign gpio_enb[pin] = {16{`IO_INPUT}}
     
 /* gpio/wb settings:
  * 0 - vcc           [I]
  * 1 - Alarm_rst     [I]
  * 2 - Alarm_ctr_rst [I]
- * 3 - Alarm         [O]
- * 4 - Alarm_latch   [O]
- * 5:12 - Alarm_ctr  [O]
+ * 3 - Glitch        [I]
+ * 4 - Alarm         [O]
+ * 5 - Alarm_latch   [O]
+ * 6:13 - Alarm_ctr  [O]
+ * 15:14 - PN_select [I]
  *
  * design options:
  * 0 - only positive detector
  * 1 - only negative detector
  * 2 - both detectors
  */
+ 
+module hp_glitcher #(
+    parameter GLITCH_OFF = 137,
+    parameter GLITCH0 = GLITCH_OFF + 3,
+    parameter GLITCH1 = GLITCH_OFF + 5,
+    parameter GLITCH2 = GLITCH_OFF + 7,
+    parameter GLITCH3 = GLITCH_OFF + 11,
+    parameter GLITCH4 = GLITCH_OFF + 13,
+    parameter GLITCH5 = GLITCH_OFF + 17,
+    parameter GLITCH6 = GLITCH_OFF + 19,
+    parameter GLITCH7 = GLITCH_OFF + 23
+) (
+    input  wire clk,
+    input  wire reset,
+    input  wire enable,
+    output wire glitch
+);
+
+reg [19:0] counter = 0;
+reg glitch_r = 0;
+always @(posedge clk)
+begin
+    glitch_r <= 0;
+    if (reset | !enable)
+        counter <= 0;
+    else if (enable)
+    begin
+        counter <= counter + 1;
+        case(counter)
+        GLITCH0: glitch_r <= 1;
+        GLITCH1: glitch_r <= 1;
+        GLITCH2: glitch_r <= 1;
+        GLITCH3: glitch_r <= 1;
+        GLITCH4: glitch_r <= 1;
+        GLITCH5: glitch_r <= 1;
+        GLITCH6: glitch_r <= 1;
+        GLITCH7: glitch_r <= 1;
+        endcase
+    end
+end
+
+assign glitch = glitch_r & !clk;
+
+endmodule
 
 module wb_hp #(
     parameter   [1:0]   DESIGN             = 2,
-    parameter   [31:0]  BASE_ADDRESS       = 32'h3000_0000        // base address
+    parameter   [31:0]  BASE_ADDRESS       = 32'h3000_0000,        // base address
+    parameter           ENABLE_GLITCH      = 1
 ) (
 `ifdef USE_POWER_PINS
     inout  wire vdda1,	// User area 1 3.3V supply
@@ -54,55 +101,49 @@ module wb_hp #(
     output reg  [31:0]   o_wb_data,      // output data
 
     // buttons
-    input  wire [12:0]   gpio_i,
-    output wire [12:0]   gpio_enb,        // not enable - low for active
-    output wire [12:0]   gpio_o,
+    input  wire [15:0]   gpio_i,
+    output wire [15:0]   gpio_enb,        // not enable - low for active
+    output wire [15:0]   gpio_o,
     
-    // glitch test input, hard wire to 0 to disable
-    input wire           glitch
+    output wire          glitch
 );
        
 wire hp_Alarm;
-wire hp_vcc;
-reg wb_hp_vcc;
+wire hp_vcc, hp_glitch_en;
+reg wb_hp_vcc, wb_hp_glitch_en;
+wire [1:0] hp_pn_select;
+reg [1:0] wb_pn_select;
 
 generate
-    if(DESIGN == 0)
+    if(ENABLE_GLITCH)
     begin
-        hoggephase #(0) hoggephase (
-            .CK(clk),
-            .VCC(hp_vcc | wb_hp_vcc),
-            .Alarm(hp_Alarm),
+        hp_glitcher hp_glitcher (
+            .clk(clk),
+            .reset(reset),
+            .enable(hp_glitch_en | wb_hp_glitch_en),
             .glitch(glitch)
         );
     end
-    else if(DESIGN == 1)
-    begin
-        hoggephase #(1) hoggephase (
-            .CK(clk),
-            .VCC(hp_vcc | wb_hp_vcc),
-            .Alarm(hp_Alarm),
-            .glitch(glitch)
-        );
-    end
-    else if(DESIGN == 2)
-    begin
-        wire hp_Alarm_p, hp_Alarm_n;
-        hoggephase #(0) hoggephase_p (
-            .CK(clk),
-            .VCC(hp_vcc | wb_hp_vcc),
-            .Alarm(hp_Alarm_p),
-            .glitch(glitch)
-        );
-        hoggephase #(1) hoggephase_n (
-            .CK(clk),
-            .VCC(hp_vcc | wb_hp_vcc),
-            .Alarm(hp_Alarm_n),
-            .glitch(glitch)
-        );
-        assign hp_Alarm = hp_Alarm_p | hp_Alarm_n;
-    end
+    else
+        assign glitch = 0;
 endgenerate
+
+wire hp_Alarm_p, hp_Alarm_n;
+hoggephase #(0) hoggephase_p (
+    .CK(clk),
+    .VCC(hp_vcc | wb_hp_vcc),
+    .Alarm(hp_Alarm_p),
+    .glitch(glitch)
+);
+hoggephase #(1) hoggephase_n (
+    .CK(clk),
+    .VCC(hp_vcc | wb_hp_vcc),
+    .Alarm(hp_Alarm_n),
+    .glitch(glitch)
+);
+
+// select which detectors to use...
+assign hp_Alarm = (hp_pn_select[0] & hp_Alarm_p) | (hp_pn_select[1] & hp_Alarm_n);
     
 assign o_wb_stall = 0;
 
@@ -110,65 +151,82 @@ assign o_wb_stall = 0;
 reg hp_Alarm_latch = 0;
 wire hp_Alarm_rst;
 reg wb_hp_Alarm_rst = 0;
-always @(hp_Alarm or hp_Alarm_rst or wb_hp_Alarm_rst)
-    if(hp_Alarm_rst | wb_hp_Alarm_rst | reset)
+wire hp_Alarm_latch_async_rst = hp_Alarm_rst | wb_hp_Alarm_rst | reset | hp_Alarm;
+always @(posedge clk or hp_Alarm_latch_async_rst)
+begin
+    if (hp_Alarm_rst | wb_hp_Alarm_rst | reset)
         hp_Alarm_latch <= 0;
     else if(hp_Alarm)
         hp_Alarm_latch <= 1;
+end
 
 // implement counter clocked by alarm signal, this in theory shouldn't be faster than the speed of clk ?
 reg [7:0] hp_Alarm_ctr = 0;
 wire hp_Alarm_ctr_rst;
 reg wb_hp_Alarm_ctr_rst = 0;
-always @(hp_Alarm or hp_Alarm_ctr_rst or wb_hp_Alarm_ctr_rst)
-    if(hp_Alarm_ctr_rst | wb_hp_Alarm_ctr_rst | reset)
+wire hp_Alarm_ctr_async_rst = hp_Alarm_ctr_rst | wb_hp_Alarm_ctr_rst | reset | hp_Alarm;
+always @(posedge clk or hp_Alarm_ctr_async_rst)
+begin
+    if (hp_Alarm_ctr_rst | wb_hp_Alarm_ctr_rst | reset)
         hp_Alarm_ctr <= 0;
-    else if(hp_Alarm)
+    else if (hp_Alarm)
         hp_Alarm_ctr <= hp_Alarm_ctr + 1;
+end
 
 // writes
 reg [7:0] Alarm_counter = 0;
-always @(posedge clk) begin
-    if(reset)
+always @(posedge clk or reset)
+begin
+    if (reset)
     begin
-        wb_hp_vcc <= 0;
-        wb_hp_Alarm_rst <= 0;
+        wb_hp_vcc           <= 0;
+        wb_hp_Alarm_rst     <= 0;
         wb_hp_Alarm_ctr_rst <= 0;
+        wb_hp_glitch_en     <= 0;
     end
     else if(i_wb_stb && i_wb_cyc && i_wb_we && !o_wb_stall && i_wb_addr == BASE_ADDRESS) begin
         wb_hp_vcc           <= i_wb_data[0];
         wb_hp_Alarm_rst     <= i_wb_data[1];
         wb_hp_Alarm_ctr_rst <= i_wb_data[2];
+        wb_hp_glitch_en     <= i_wb_data[3];
+        wb_hp_pn_select     <= i_wb_data[15:14];
     end
 end
 
 // reads
-always @(posedge clk) begin
-    if(i_wb_stb && i_wb_cyc && !i_wb_we && !o_wb_stall && i_wb_addr == BASE_ADDRESS)
-        o_wb_data <= {19'h0, hp_Alarm_ctr, hp_Alarm_latch, hp_Alarm, wb_hp_Alarm_ctr_rst, wb_hp_Alarm_rst, wb_hp_vcc};
-    else
+always @(posedge clk or reset) begin
+    if (reset)
+    begin
         o_wb_data <= 32'h0;
+    end
+    else
+    begin
+        if (i_wb_stb && i_wb_cyc && !i_wb_we && !o_wb_stall && i_wb_addr == BASE_ADDRESS)
+            o_wb_data <= {16'h0, wb_hp_pn_select, hp_Alarm_ctr, hp_Alarm_latch, hp_Alarm, wb_hp_glitch_en, wb_hp_Alarm_ctr_rst, wb_hp_Alarm_rst, wb_hp_vcc};
+        else
+            o_wb_data <= 32'h0;
+    end
 end
 
 // acks
-always @(posedge clk) begin
-    if(reset)
+always @(posedge clk or reset) begin
+    if (reset)
         o_wb_ack <= 0;
     else
         // return ack immediately
-        o_wb_ack <= (i_wb_stb && !o_wb_stall && (i_wb_addr[31:8] == BASE_ADDRESS));
+        o_wb_ack <= (i_wb_stb && !o_wb_stall && (i_wb_addr == BASE_ADDRESS));
 end
 
-always @(posedge clk) begin
-    // gpio inputs
-    `SET_IO_INPUT  (hp_vcc,           0);
-    `SET_IO_INPUT  (hp_Alarm_rst,     1);
-    `SET_IO_INPUT  (hp_Alarm_ctr_rst, 2);
+// gpio inputs
+`SET_IO_INPUT  (hp_vcc,           0);
+`SET_IO_INPUT  (hp_Alarm_rst,     1);
+`SET_IO_INPUT  (hp_Alarm_ctr_rst, 2);
+`SET_IO_INPUT  (hp_glitch_en,     3);
+`SET_IO_INPUT  (hp_pn_select,     15:14);
     
-    // gpio outputs
-    `SET_IO_OUTPUT (hp_Alarm,         3);
-    `SET_IO_OUTPUT (hp_Alarm_latch,   4);
-    `SET_IO_OUTPUT (hp_Alarm_ctr,     12:5);
-end
+// gpio outputs
+`SET_IO_OUTPUT (hp_Alarm,         4);
+`SET_IO_OUTPUT (hp_Alarm_latch,   5);
+`SET_IO_OUTPUT (hp_Alarm_ctr,     13:6);
 
 endmodule
